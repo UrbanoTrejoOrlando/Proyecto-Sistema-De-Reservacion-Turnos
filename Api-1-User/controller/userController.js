ffa6d5d8ba71aaa7deecf15373e63a70c8ba2a7f
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-24 21:14:37 -0600

    Modificaciones

--- Api-1-User/controller/userController.js
+++ Api-1-User/controller/userController.js
@@ -4,12 +4,22 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken")
 
 // Peticion para crear un nuevo usuario
-const createContact = async (req, res) =>{
+const createUser = async (req, res) =>{
     try {
         // Guardar los datos del body en una variable
         const userData = req.body;
+
+        //Verificar si la contraseña se ingreso
+        if(!userData.contrasenia){
+            return res.status(400).json({ error: "La contraseña es obligatoria" });
+        }
+
+        
+
+
+
         // Crear al contacto
-        const newUser = await contactServices.CreateContact(userData);
+        const newUser = await contactServices.CreateUser(userData);
         // Configuracion del json
         res.status(201).json({
             message: "Contacto creado correctamente",
