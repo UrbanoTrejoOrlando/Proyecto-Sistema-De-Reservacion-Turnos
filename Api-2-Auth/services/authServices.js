611ecd3d84d9a888d3e38b52cbb7616ffda77e50
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-29 10:58:41 -0600

    Modificaciones

--- Api-2-Auth/services/authServices.js
+++ Api-2-Auth/services/authServices.js
@@ -1,10 +1,12 @@
 // Importacion de librerias
-const { axios } = require("axios");
+const axios  = require("axios");
 require("dotenv").config()
 
-const URL_API = process.env.URL_API;
+const URL_API = process.env.URL_USERS;
 
 // Funcion para obtener todos los usuarios desde la api externa
-const getUser = async ()=>{
-    const usuarios = await 
+const getUsers = async ()=>{
+    const usuarios = await axios.get(URL_API);
+    return usuarios.data;
 }
+module.exports ={getUsers};
