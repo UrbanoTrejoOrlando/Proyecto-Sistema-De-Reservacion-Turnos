1e160d6725891e44094a19ce57f09fc73ea31be7
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-29 18:57:36 -0600

    Carpeta config

---
+++ Api-Gateway/config/services.js
@@ -0,0 +1,10 @@
+// Definicion de las rutas de las Apis
+const services =[
+    {
+        //APi 1
+        name: "usuarios",
+        url:  "http://localhost:3006",
+        path: "/api-usuarios"
+    }
+
+]
