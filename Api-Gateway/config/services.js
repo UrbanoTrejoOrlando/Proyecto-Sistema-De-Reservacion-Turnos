627b1bc20ce4d58817240df0ef48bed301c22513
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-29 18:59:13 -0600

    Modificaciones

--- Api-Gateway/config/services.js
+++ Api-Gateway/config/services.js
@@ -1,10 +1,16 @@
 // Definicion de las rutas de las Apis
 const services =[
     {
-        //APi 1
+        //Api 1
         name: "usuarios",
         url:  "http://localhost:3006",
         path: "/api-usuarios"
-    }
+    },
+    {
+        // Api 2
+        name: "auth",
+        url:  "http://localhost:3007",
+        path: "/api-login"
 
+    }
 ]
