1faee931467d781a6779b8af153ee9b45b52d1de
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-29 19:22:17 -0600

    Carpeta middlewares

--- Api-Gateway/middlewares/logger.js
+++ Api-Gateway/middlewares/logger.js
@@ -1,1 +1,13 @@
 // Funcion que representa un middleware
+const logger = (req, res, next)=>{
+    // Obtiene la fecha en tiempo real
+    const fecha = new Date().toDateString();
+    // Obtencion de todas las peticiones 
+    const method = req.method;
+    const url = req.originalUrl;
+
+console.log(`[${fecha}]${method} ${url}`);
+next();
+}
+
+module.exports = logger
