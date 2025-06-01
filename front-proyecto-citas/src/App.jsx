eb1e3276c9a120154cd0cb9217f2e9ae948c1ed4
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-06-01 15:01:59 -0600

    MOdificaciones

--- front-proyecto-citas/src/App.jsx
+++ front-proyecto-citas/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './Login/Login'
 import RegistroUsuarios from './RegistroUsuarios/RegistroUsuarios'
-import {administrador} from " ./";
+import Administrador from './Administrador/Administrador'
+import Cliente from './Cliente/Cliente'
 
 
 const App = () => {
@@ -11,7 +12,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login/>}/>
         <Route path="/registro" element={<RegistroUsuarios/>}/>
-        <Route path="/administrador" element={<administrador/>}/>
+        <Route path="/administrador" element={<Administrador/>}/>
+        <Route path="/cliente" element={<Cliente/>}/>
 
 
       </Routes>
