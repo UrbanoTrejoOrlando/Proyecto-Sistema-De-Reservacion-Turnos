f039768e1c31e78a6d564a778b79263b6543961d
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-31 22:48:55 -0600

    Modificaciones

--- front-proyecto-citas/src/App.jsx
+++ front-proyecto-citas/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './Login/Login'
+import RegistroUsuarios from './RegistroUsuarios/RegistroUsuarios'
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route index element={<Login/>}/>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/registro" element={<RegistroUsuarios/>}/>
       </Routes>
     </Router>
   )
