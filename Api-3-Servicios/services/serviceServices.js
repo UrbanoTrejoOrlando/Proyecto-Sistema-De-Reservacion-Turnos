95210988fc4a292e2d78f8e9d145360d88802600
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-06-04 11:35:16 -0600

    Modificaciones

--- Api-3-Servicios/services/serviceServices.js
+++ Api-3-Servicios/services/serviceServices.js
@@ -2,18 +2,18 @@
 const Citas = require("../model/serviceModel");
 
 //Funcion para crear un contacto
-const CreateCita = async (data)=>{
+const CreateService = async (data)=>{
     try{
         // Creacion de un usuario
-        const newCita = new Citas(data);
+        const newService = new Citas(data);
         // Guardar el regsitro en la base de datos
-        await newCita.save()
+        await newServices.save()
         // Retornar el nuevo usuario
-        return newCita;
+        return newService;
 
     }catch(error){  
         // Mensaje de errro si algo falla
-        throw new Error("Error al crear el usuario" + error.message);
+        throw new Error("Error al crear el servicio" + error.message);
 
     }
 };
