94fec7cc1f88808240fd9f1cb223996e1550c5d4
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-06-04 10:09:20 -0600

    Carpeta model

--- Api-3-Servicios/model/citaModel.js
+++ Api-3-Servicios/model/citaModel.js
@@ -1,3 +1,9 @@
 // Extension del mongooe 
 const mongoose = require("mongoose");
 // Defincion de la coleecion de la base de datos
+const citaSchema = new mongoose.Schema(
+    {
+
+    }
+)
+module.exports = mongoose.model("Citas", citaSchema);
