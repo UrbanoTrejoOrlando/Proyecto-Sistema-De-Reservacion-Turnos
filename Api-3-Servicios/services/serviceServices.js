92414091b0e12debe627c78feae57f4729486a30
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-06-04 11:39:12 -0600

    Modificaciones

--- Api-3-Servicios/services/serviceServices.js
+++ Api-3-Servicios/services/serviceServices.js
@@ -1,11 +1,11 @@
 // Variable para definir el modelo de base de datos
-const Citas = require("../model/serviceModel");
+const Services = require("../model/serviceModel");
 
-//Funcion para crear un contacto
+//Funcion para crear un servicio
 const CreateService = async (data)=>{
     try{
         // Creacion de un usuario
-        const newService = new Citas(data);
+        const newService = new Services(data);
         // Guardar el regsitro en la base de datos
         await newServices.save()
         // Retornar el nuevo usuario
@@ -15,5 +15,34 @@ const CreateService = async (data)=>{
         // Mensaje de errro si algo falla
         throw new Error("Error al crear el servicio" + error.message);
 
+    }
+};
+
+// Funcion para obtener un servicio
+const GetAllServices = async() =>{
+    try{
+        // Obtener el servicio
+        const services = await Services.find();
+        // Retornar servicios
+        return services;
+    }catch(error){
+        // Mensaje de error si algo falla
+        throw new Error("Error al obtener los servicios" + error.message);
+    }    
+};
+
+// Funcion para obtener un servicio por su Id
+const GetServiceById = async(serviceid)=>{
+    try {
+        // Obtencion del servicio
+        const service = await Usuarios.findById(userid);
+        // Validar si el usuario existe 
+        if(!user) throw new Error("El usuario no existe "+ error.message);
+        // retorna el usuarios
+        return user;
+
+    } catch (error) {
+        // Mensaje de errro si algo falla
+        throw new Error("Error al obtener el usuario" + error.message);
     }
 };
