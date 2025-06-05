0015fdaeab6e9e233ce73f37410008f9fe560161
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-06-05 07:23:11 -0600

    Index

---
+++ Api-3-Servicios/index.js
@@ -0,0 +1,26 @@
+// importacion del paquete de express
+const express = require("express");
+// importacion del paquete de cors
+const cors = require("cors");
+// conexion de la base de datos
+const {ConnectDB} = require("./data/config");
+// importacion de las rutas
+const userRouter = require("./routes/serviceRoutes");
+// Importacion dotenv
+require("dotenv").config();
+// Definicion del puerto 
+const PORT = process.env.PORT
+
+// Creacion de la instancia del servidor
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api-3-services',userRouter);
+//Agregar la conexion a la base de datos
+ConnectDB();
+// Eecucion del servidor
+app.listen(PORT, ()=>{
+    console.log("Server running in http://localhost:"+PORT)
+});
+
