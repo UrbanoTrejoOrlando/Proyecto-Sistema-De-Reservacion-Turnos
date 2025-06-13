c9e097a589e46b10f57fc0ef83ab233a33ed7ac8
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-06-13 14:34:21 -0600

    Modificaciones

--- front-proyecto-citas/src/App.jsx
+++ front-proyecto-citas/src/App.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './Login/Login'
-import RegistroUsuarios from './RegistroUsuarios/RegistroUsuarios'
-import Administrador from './Administrador/Administrador'
-import Cliente from './Cliente/Cliente'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import AuthPage from './Login/AuthPage';
+import Administrador from './Administrador/Administrador';
+import Cliente from './Cliente/Cliente';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/registro" element={<RegistroUsuarios/>}/>
-        <Route path="/administrador" element={<Administrador/>}/>
-        <Route path="/cliente" element={<Cliente/>}/>
-
-
+        <Route path="/" element={<AuthPage />} /> 
+        <Route path="/login" element={<AuthPage />} /> 
+        <Route path="/registro" element={<AuthPage />} /> 
+        <Route path="/administrador" element={<Administrador />} />
+        <Route path="/cliente" element={<Cliente />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
