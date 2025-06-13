9d84a29c18f523e8877389ea6ed8d473addf6a86
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-06-13 14:34:44 -0600

    Modificaciones

--- front-proyecto-citas/src/common/server.js
+++ front-proyecto-citas/src/common/server.js
@@ -1,8 +1,9 @@
 // Creacion de url de donde se consume la Api-1-usuarios
-const ApiUsuarios = "http://localhost:3020/api-usuarios/api-1-user/users"
+//const ApiUsuarios = "http://localhost:3020/api-usuarios/api-1-user/users"
+const ApiUsuarios = "http://localhost:3006/api-1-user/users"
 
 // Segunda url para generar el login
-const ApiAuth = "http://localhost:3020/api-login/api-2-auth/login"
+const ApiAuth = "http://localhost:3007/api-2-auth/login"
 
 // Exportacion de url
 export {ApiUsuarios, ApiAuth};
