eda5cfde8270ae8ed9f238eee7e3d9eadde674a4
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-06-13 14:33:55 -0600

    Modificaciones

--- Api-2-Auth/index.js
+++ Api-2-Auth/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+// importacion del paquete de cors
+const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
 
 app.use(express.json());
+app.use(cors());
 app.use("/api-2-auth", authRoutes);
 
 const PORT = process.env.PORT || 3007;
