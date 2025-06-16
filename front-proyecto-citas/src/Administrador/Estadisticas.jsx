9e1708d182b790e7df942e60c6ded463bb88c013
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-06-16 13:21:59 -0600

    Modificaciones

--- front-proyecto-citas/src/Administrador/Estadisticas.jsx
+++ front-proyecto-citas/src/Administrador/Estadisticas.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Estadisticas = () => {
   return (
-    <div>
+    <div className="space-y-8">
+        <div>
+        <h1 className="text-3xl font-bold text-gray-900 mb-2">Panel de Estadísticas</h1>
+        <p className="text-gray-600">Resumen general del sistema de citas médicas</p>
+      </div>
       
     </div>
   )
