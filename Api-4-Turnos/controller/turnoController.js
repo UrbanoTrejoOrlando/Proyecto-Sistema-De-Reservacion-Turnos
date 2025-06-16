7a2bcd8037e622beb3f52b6993eef854087ac622
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-06-15 22:08:53 -0600

    Controller

---
+++ Api-4-Turnos/controller/turnoController.js
@@ -0,0 +1,63 @@
+const TurnoService = require("../services/turnoServices");
+
+const createTurno = async (req, res) => {
+  try {
+    const turno = await TurnoService.crearTurno(
+      req.body,
+      req.user.userId,
+      req.headers.authorization?.split(" ")[1],
+      req.io
+    );
+    res.status(201).json(turno);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const cancelTurno = async (req, res) => {
+  try {
+    const turno = await TurnoService.cancelarTurno(
+      req.params.id,
+      req.user.userId,
+      req.headers.authorization?.split(" ")[1],
+      req.io
+    );
+    res.json(turno);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const listTurnos = async (req, res) => {
+  try {
+    const turnos = await TurnoService.listarTurnos(
+      req.query,
+      req.user.userId,
+      req.headers.authorization?.split(" ")[1]
+    );
+    res.json(turnos);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const getAvailableTurnos = async (req, res) => {
+  try {
+    const { servicioId, fecha } = req.query;
+    const horarios = await TurnoService.generarTurnosDisponibles(
+      servicioId,
+      fecha,
+      req.headers.authorization?.split(" ")[1]
+    );
+    res.json(horarios);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createTurno,
+  cancelTurno,
+  listTurnos,
+  getAvailableTurnos,
+};
